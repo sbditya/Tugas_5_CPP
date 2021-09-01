959c292d7ad9f6f8874608575e0c3cad6b7238f1
Salsabila Ditya Anisa <sbditya320@gmail.com>
2021-09-01 13:03:11 +0700

    Tugas 5 CPP Niomic
    Salsabila Ditya Anisa

---
+++ tugas 5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std; 
+
+int main(){
+	float s;
+	cout<<"Masukan Nilai nya dulu : ";
+	cin>>s;
+	s = pow(s,3);
+	cout<<"Hasil pangkat tiga nya adalah : "<<s;
+}
